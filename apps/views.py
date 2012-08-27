875d3fff1e439e468014c09391a73d548f6dc5d7
Stepa <steeg.xs@gmail.com>
2012-08-27 22:26:10 +1000

    статистика чёртова)

--- apps/views.py
+++ apps/views.py
@@ -242,123 +242,142 @@ class DBCopyView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super(DBCopyView, self).get_context_data(**kwargs)
 
-#        rows = MobileInternetSpeed.objects.all()
-#        #будут районы
-#        routes = MobileInternetSpeed.objects.values('Route').distinct().order_by('Route')
-#        curr_city = City.objects.get(id=1)
-#        for route in routes:
-#            title = route['Route']
-#            if title==None:
-#                pass
-#            else:
-#                exits = Distinct.objects.filter(title=title)
-#                if not exits:
-#                    new_distinct = Distinct(city=curr_city, title=title)
-#                    new_distinct.save()
-#
-#        for row in rows:
-#            try:
-#                distinct = Distinct.objects.get(title=row.Route)
-#            except:
-#                distinct = False
-#            coords = u'%s,%s' % (row.Latitude, row.Longitude)
-#            if distinct:
-#                try:
-#                    point = Point.objects.get(coord=coords)
-#                except:
-#                    point = False
-#                if point:
-#                    new_point = point
-#                    '''speed_values = point.get_speed_values()
-#                    for value in speed_values:
-#                        if value.modem_type_id==row.megafonModemTypeId:
-#                            if value.internet_speed < row.megafonDownloadSpeed:
-#                                value.internet_speed = row.megafonDownloadSpeed
-#                                value.save()
-#                        if value.modem_type_id==row.mtsModemTypeId:
-#                            if value.internet_speed < row.mtsDownloadSpeed:
-#                                value.internet_speed = row.mtsDownloadSpeed
-#                                value.save()
-#                        if value.modem_type_id==row.beelineModemTypeId:
-#                            if value.internet_speed < row.beelineDownloadSpeed:
-#                                value.internet_speed = row.beelineDownloadSpeed
-#                                value.save()'''
-#                else:
-#                    new_point = Point(distinct=distinct, coord=coords, datetime_create=row.currentMoment)
-#                    new_point.save()
-#                megafon_mtype = ModemType.objects.get(id=row.megafonModemTypeId)
-#                megafon_mtype_operator = megafon_mtype.operator
-#                mts_mtype = ModemType.objects.get(id=row.mtsModemTypeId)
-#                mts_mtype_operator = mts_mtype.operator
-#                beeline_mtype = ModemType.objects.get(id=row.beelineModemTypeId)
-#                beeline_mtype_operator = beeline_mtype.operator
-#
-#                new_pt_spd_meg = SpeedAtPoint(point=new_point, operator=megafon_mtype_operator, modem_type=megafon_mtype, internet_speed=row.megafonDownloadSpeed )
-#                new_pt_spd_mts = SpeedAtPoint(point=new_point, operator=mts_mtype_operator, modem_type=mts_mtype, internet_speed=row.mtsDownloadSpeed )
-#                new_pt_spd_beel = SpeedAtPoint(point=new_point, operator=beeline_mtype_operator, modem_type=beeline_mtype, internet_speed=row.beelineDownloadSpeed )
-#                new_pt_spd_meg.save()
-#                new_pt_spd_mts.save()
-#                new_pt_spd_beel.save()
-#
-#        context['result'] = rows.count
+        not_distincts = []
+        same_coord = []
+        rows = MobileInternetSpeed.objects.all()
+        #будут районы
+        routes = MobileInternetSpeed.objects.values('Route').distinct().order_by('Route')
+        curr_city = City.objects.get(id=1)
+        for route in routes:
+            title = route['Route']
+            if title==None:
+                pass
+            else:
+                exits = Distinct.objects.filter(title=title)
+                if not exits:
+                    new_distinct = Distinct(city=curr_city, title=title)
+                    new_distinct.save()
+
+        for row in rows:
+            try:
+                distinct = Distinct.objects.get(title=row.Route)
+            except:
+
+                ###
+                not_distincts.append(row.currentMoment)
+                ###
+
+                distinct = False
+            coords = u'%s,%s' % (row.Latitude, row.Longitude)
+            if distinct:
+                try:
+                    point = Point.objects.get(coord=coords)
+                except:
+                    point = False
+                if point:
+
+                    ###
+                    list = []
+                    list.append(row.currentMoment)
+                    list.append(row.Latitude)
+                    list.append(row.Longitude)
+                    list.append(point.datetime_create)
+                    same_coord.append(list)
+                    ###
+
+                    new_point = point
+                    '''speed_values = point.get_speed_values()
+                    for value in speed_values:
+                        if value.modem_type_id==row.megafonModemTypeId:
+                            if value.internet_speed < row.megafonDownloadSpeed:
+                                value.internet_speed = row.megafonDownloadSpeed
+                                value.save()
+                        if value.modem_type_id==row.mtsModemTypeId:
+                            if value.internet_speed < row.mtsDownloadSpeed:
+                                value.internet_speed = row.mtsDownloadSpeed
+                                value.save()
+                        if value.modem_type_id==row.beelineModemTypeId:
+                            if value.internet_speed < row.beelineDownloadSpeed:
+                                value.internet_speed = row.beelineDownloadSpeed
+                                value.save()'''
+                else:
+                    new_point = Point(distinct=distinct, coord=coords, datetime_create=row.currentMoment)
+                    new_point.save()
+                megafon_mtype = ModemType.objects.get(id=row.megafonModemTypeId)
+                megafon_mtype_operator = megafon_mtype.operator
+                mts_mtype = ModemType.objects.get(id=row.mtsModemTypeId)
+                mts_mtype_operator = mts_mtype.operator
+                beeline_mtype = ModemType.objects.get(id=row.beelineModemTypeId)
+                beeline_mtype_operator = beeline_mtype.operator
+
+                new_pt_spd_meg = SpeedAtPoint(point=new_point, operator=megafon_mtype_operator, modem_type=megafon_mtype, internet_speed=row.megafonDownloadSpeed )
+                new_pt_spd_mts = SpeedAtPoint(point=new_point, operator=mts_mtype_operator, modem_type=mts_mtype, internet_speed=row.mtsDownloadSpeed )
+                new_pt_spd_beel = SpeedAtPoint(point=new_point, operator=beeline_mtype_operator, modem_type=beeline_mtype, internet_speed=row.beelineDownloadSpeed )
+                new_pt_spd_meg.save()
+                new_pt_spd_mts.save()
+                new_pt_spd_beel.save()
+
+        context['result'] = rows.count
+        context['nondist'] = not_distincts
+        context['samecoord'] = same_coord
 
         # примерное расстояние в 50 метров в координатах 0.0008982 - по долготе
         # примерное расстояние в 100 метров в координатах 0.0008982 - по широте - сделовательно примерную окружность с радиусом 50 м нужно строить в виде эллипса - потому что в виду географических проекций и формы земли - построенная окружность превратится в эллпис
 
-        points = Point.objects.all().order_by('distinct')
-        R = 0.0008982 * 2
-        for curr_point in points:
-            setattr(curr_point, 'is_delete', False)
-
-        for curr_point in points:
-            if curr_point.is_delete != True:
-                curr_point_speed_values = curr_point.get_speed_values()
-                for curr_value in curr_point_speed_values:
-                    setattr(curr_value, 'avg_set', [curr_value.internet_speed, ])
-                curr_point_coord = curr_point.coord.split(',')
-                curr_point_coord = [float(curr_point_coord[0]), float(curr_point_coord[1])]
-                for point in points:
-                    if point.id != curr_point.id:
-                        next_point_coord = point.coord.split(',')
-                        next_point_coord = [float(next_point_coord[0]), float(next_point_coord[1])]
-                        #circle = (next_point_coord[0] - curr_point_coord[0])**2 + (next_point_coord[1] - curr_point_coord[1])**2
-                        ellipse = ((next_point_coord[0] - curr_point_coord[0]) ** 2 / ((R / 2) ** 2)) + ((next_point_coord[1] - curr_point_coord[1]) ** 2 / (R ** 2))
-                        #if circle <= R**2:
-                        if ellipse <= 1:
-                            speed_values = point.get_speed_values()
-                            for curr_value in curr_point_speed_values:
-                                for value in speed_values:
-                                    value.point = curr_point
-                                    value.save()
-                                    '''if value.modem_type_id == curr_value.modem_type_id: # если типы модемов для точек равны - в усреднение
-                                        curr_value.avg_set.append(value.internet_speed)
-                                        #curr_value.internet_speed = (curr_value.internet_speed + value.internet_speed) / 2
-                                        #curr_value.save()
-                                    else: # если не равны то к текущей точке добавляем данные о замере скорости
-                                        new_measuring_data = SpeedAtPoint(point=curr_point, operator=value.operator, modem_type=value.modem_type, internet_speed=value.internet_speed)
-                                        new_measuring_data.save()'''
-                            setattr(point, 'is_delete', True)
-                        else:
-                            pass
-                    # найдём среднюю скорость, и сохраним
-                '''for curr_value in curr_point_speed_values:
-                    summ = 0
-                    counter = 0
-                    for val in curr_value.avg_set:
-                        summ = summ + val
-                        counter = counter + 1
-                    curr_value.internet_speed = summ / counter
-                    curr_value.save()'''
-            else:
-                ex = 1
-
-        for point in points:
-            speed_values = point.get_speed_values()
-            if point.is_delete == True: # удаляем
-                for value in speed_values:
-                    value.delete()
-                point.delete()
-
+#        points = Point.objects.all().order_by('distinct')
+#        R = 0.0008982 * 2
+#        for curr_point in points:
+#            setattr(curr_point, 'is_delete', False)
+#
+#        for curr_point in points:
+#            if curr_point.is_delete != True:
+#                curr_point_speed_values = curr_point.get_speed_values()
+#                for curr_value in curr_point_speed_values:
+#                    setattr(curr_value, 'avg_set', [curr_value.internet_speed, ])
+#                curr_point_coord = curr_point.coord.split(',')
+#                curr_point_coord = [float(curr_point_coord[0]), float(curr_point_coord[1])]
+#                for point in points:
+#                    if point.id != curr_point.id:
+#                        next_point_coord = point.coord.split(',')
+#                        next_point_coord = [float(next_point_coord[0]), float(next_point_coord[1])]
+#                        #circle = (next_point_coord[0] - curr_point_coord[0])**2 + (next_point_coord[1] - curr_point_coord[1])**2
+#                        ellipse = ((next_point_coord[0] - curr_point_coord[0]) ** 2 / ((R / 2) ** 2)) + ((next_point_coord[1] - curr_point_coord[1]) ** 2 / (R ** 2))
+#                        #if circle <= R**2:
+#                        if ellipse <= 1:
+#                            speed_values = point.get_speed_values()
+#                            for curr_value in curr_point_speed_values:
+#                                for value in speed_values:
+#                                    value.point = curr_point
+#                                    value.save()
+#                                    '''if value.modem_type_id == curr_value.modem_type_id: # если типы модемов для точек равны - в усреднение
+#                                        curr_value.avg_set.append(value.internet_speed)
+#                                        #curr_value.internet_speed = (curr_value.internet_speed + value.internet_speed) / 2
+#                                        #curr_value.save()
+#                                    else: # если не равны то к текущей точке добавляем данные о замере скорости
+#                                        new_measuring_data = SpeedAtPoint(point=curr_point, operator=value.operator, modem_type=value.modem_type, internet_speed=value.internet_speed)
+#                                        new_measuring_data.save()'''
+#                            setattr(point, 'is_delete', True)
+#                        else:
+#                            pass
+#                    # найдём среднюю скорость, и сохраним
+#                '''for curr_value in curr_point_speed_values:
+#                    summ = 0
+#                    counter = 0
+#                    for val in curr_value.avg_set:
+#                        summ = summ + val
+#                        counter = counter + 1
+#                    curr_value.internet_speed = summ / counter
+#                    curr_value.save()'''
+#            else:
+#                ex = 1
+#
+#        for point in points:
+#            speed_values = point.get_speed_values()
+#            if point.is_delete == True: # удаляем
+#                for value in speed_values:
+#                    value.delete()
+#                point.delete()
+#
 
         # усредним замеры по повторяющимся типам модемов
 #        for curr_point in points:
